5fb1fc01efd4ec5486e0178c919ea0cebecb7d84
chennxi123 <104598652+chennxi123@users.noreply.github.com>
2022-05-07 15:24:00 -0400

    Add files via upload

---
+++ src/components/home/MainBody.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Typed from "react-typed";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+const MainBody = React.forwardRef(
+  ({ gradient, title, message, icons }, ref) => {
+    return (
+      <Jumbotron
+        fluid
+        id="home"
+        style={{
+          // background: `linear-gradient(136deg,${gradient})`,
+          backgroundSize: "1200% 1200%",
+        }}
+        className="title bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
+      >
+        <div id="stars"></div>
+        <Container className="text-center ">
+          <h1 ref={ref} className="display-1 nameHeading ">
+            {title}
+          </h1>
+
+          {/* <Typist className="lead typist" cursor={{ show: false }}>
+            {" "}
+            {message}
+          </Typist> */}
+
+          <Typed
+            className="typed-text lead typist"
+            strings={[
+              "Competitive Programmer",
+              "Full Stack Developer",
+              "Open Source Contributor",
+              "DS-Algo Expert",
+              "Problem Solver",
+              "IIIT Gwalior'23",
+            ]}
+            typeSpeed={80}
+            backSpeed={10}
+            loop
+          ></Typed>
+
+          <div className="p-5">
+            {icons.map((icon, index) => (
+              <a
+                // className=""
+                key={`social-icon-${index}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={icon.url}
+                aria-label={`My ${icon.image.split("-")[1]}`}
+              >
+                <i className={`fab ${icon.image}  fa-3x socialicons`} />
+              </a>
+            ))}
+          </div>
+
+          <a
+            className="btn btn-outline-light btn-lg "
+            href="#aboutme"
+            role="button"
+            aria-label="Learn more about me"
+          >
+            More about me
+          </a>
+        </Container>
+      </Jumbotron>
+    );
+  }
+);
+
+export default MainBody;
